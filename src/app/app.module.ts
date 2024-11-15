f17fb81dd626a23908eadd0edb7613ce48ecd205
silviachaluisa <veronica-14-1998@hotmail.com>
2024-11-14 22:04:49 -0500

    Ultimos Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,15 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 //import { firebaseConfig as environment } from 'src/environments/environment';
-import {firebase as environment} from 'src/environments/environment';
+import { firebaseConfig } from 'src/environments/environment';
+// import {firebase as environment} from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
-    provideFirebaseApp(() => initializeApp(environment)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+    provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+ 
+    apiKey: "AIzaSyDGB_YqWXpK9S2zWmnXJvrW7cPmDmRPE-E",
+    authDomain: "mi-app-e05b4.firebaseapp.com",
+    projectId: "mi-app-e05b4",
+    storageBucket: "mi-app-e05b4.firebasestorage.app",
+    messagingSenderId: "291298671442",
+    appId: "1:291298671442:web:c7d7f185fdf55d56e8588b",
+    measurementId: "G-YGJT1P9KG5"
+  
+};
